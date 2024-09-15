dad256a5b446b89eb339f4f362ce1ec291519474
kumargoud <desham.kumargoud1994@gmail.com>
2024-09-15 10:36:18 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_security_group" "allow_ssh_terraform" {
-  name        = "allow_ssh" #allow_ssh is already there in my account
-  description = "Allow port number 22 for SSH access"
+  name        = var.sg_name
+  description = var.sg_description
   
   #usually we allow everything in egress
+  #block
   egress {
        from_port        = 0
        to_port          = 0
@@ -18,15 +19,14 @@ resource "aws_security_group" "allow_ssh_terraform" {
       cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
       ipv6_cidr_blocks = ["::/0"]
     }
-    tags = {
-    Name = "allow_sshh"
+    tags = var.tags
+    
   }
-}
 
 resource "aws_instance" "terraform" {
 
-  ami = "ami-09c813fb71547fc4f"
-  instance_type = "t2.micro"
+  ami = "var.ami_id" # left side and right side names need not to be same
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
   tags = {
     Name = "terraform"
